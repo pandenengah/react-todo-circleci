89794128b23de01fba6637f3a1b0b8d7e4ad6946
Pande Nengah <PandeNengah.Purnawan@mitrais.com>
2023-01-02 11:51:53 +0800

    add amplify.yml

--- src/pages/todo/components/Footer.tsx
+++ src/pages/todo/components/Footer.tsx
@@ -4,7 +4,7 @@ export class Footer extends Component {
     render() {
         return (
             <div className="flex justify-center py-[1em]">
-                <p className="text-[0.875em] text-purple-900">&copy; Pande Nengah | React Orange Belt Exercise</p>
+                <p className="text-[0.875em] text-purple-900">&copy; Pande Nengah | React Green Belt Exercise</p>
             </div>
         )
     }
